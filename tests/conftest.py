a819ba99f1f94501706dc61dd1092e3216ded8eb
Irina Lavrenteva <lis201002@gmail.com>
2025-12-09 00:11:26 +0300

    add base implementation (2)

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='function')
 def browser_setup():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
